15f31a4453f0224c03efaba38b38f7f5c4085a71
llandela <66991825+llandela@users.noreply.github.com>
2020-06-26 11:25:48 +0200

    Add files via upload

---
+++ ft_swap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llandela <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/22 11:32:41 by llandela          #+#    #+#             */
+/*   Updated: 2020/06/24 16:46:53 by llandela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void 	ft_putchar(char ptr);
+void 	ft_putnbr(int ptr);
+void 	ft_putaddr(void *ptr);
+
+int 	main(void);
+{
+void 	ft_swap(int *a, int *b);
+
+ 	*a = *b
+	*b = *nbr
+	*nbr = *a
+}
+{
+	int 	*a;
+	int 	*b;
+	int 	*ptr;
+
+	
+	return (0);
+}
+
